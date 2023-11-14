24603d9461e31fd2944c689198419c6eca5fcb80
hdy <fwangdo35@gmail.com>
2023-11-14 15:09:40 +0900

    feat(main): Add script to fuzz models in local env

---
+++ main.py
@@ -0,0 +1,43 @@
+'''testing neural networks.
+'''
+from __future__ import annotations
+from typing     import *
+
+import os, sys
+import argparse
+
+import tensorflow as tf
+
+
+def set_gpu() -> None:
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            tf.config.experimental.set_memory_growth(gpus[0], True)
+        except RuntimeError as e:
+            print(e)
+    return
+
+
+class Executer:
+    
+    def __init__(self
+                 , target_model: str
+                 , mode: str):
+        self.target_model = target_model
+        self.mode = mode
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu', default=False)
+    parser.add_argument('--model', help='target model(e.g. lenet, vgg)')
+    parser.add_argument('--mode', help='method to generate adversarial attack(e.g. adapt, fgsm)')
+    
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
